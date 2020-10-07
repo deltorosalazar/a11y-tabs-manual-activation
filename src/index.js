d6b68c8b056cfe817fc473007751de19037b4fd0
Manuel F. Del Toro <deltorosalazar@gmail.com>
2020-10-07 13:42:51 -0500

    approach #3, seems to be working

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ const BLOCK_CLASS = "tabs";
 const CSS_SELECTORS = {
   PANEL: `.${BLOCK_CLASS}__panel`,
   TABLIST: `.${BLOCK_CLASS}__tablist`,
-  TAB: `.${BLOCK_CLASS}__tab`
+  TAB: `.${BLOCK_CLASS}__tab`,
+  TIMER: `.${BLOCK_CLASS}__timer`
 };
 
 class Tabs {
@@ -25,6 +26,11 @@ class Tabs {
     /**
      * @private {!Array<Element>}
      */
+    this.timers_ = [];
+
+    /**
+     * @private {!Array<Element>}
+     */
     this.panels_ = [];
 
     /**
@@ -39,19 +45,22 @@ class Tabs {
    *
    */
   initialize_() {
-    const { PANEL, TABLIST, TAB } = CSS_SELECTORS;
+    const { PANEL, TABLIST, TAB, TIMER } = CSS_SELECTORS;
 
     this.panels_ = [...document.querySelectorAll(PANEL)];
     this.tablist_ = document.querySelector(TABLIST);
     this.tabs_ = [...document.querySelectorAll(TAB)];
+    this.timers_ = [...document.querySelectorAll(TIMER)];
 
     console.log({
       panels_: this.panels_,
       tablist_: this.tablist_,
-      tabs_: this.tabs_
+      tabs_: this.tabs_,
+      timers_: this.timers_
     });
 
     this.registerEvents_();
+    this.getTabsHeight_();
   }
 
   /**
@@ -61,6 +70,37 @@ class Tabs {
   registerEvents_() {
     this.tabs_.forEach((tab, index) => {
       this.tabs_[index].addEventListener("click", this.clickTab_);
+    });
+  }
+
+  /**
+   * Gets the tabs height..
+   * @private
+   */
+  getTabsHeight_() {
+    this.tabs_.forEach((tab, index) => {
+      tab.style.removeProperty("--min-tab-height");
+      tab.style.removeProperty("--max-tab-height");
+
+      const tabHeight = tab.getBoundingClientRect().height;
+
+      const tabTitle = tab.querySelector(".tabs__tab__title");
+      const titleOffset = 16;
+
+      const tabTitleHeight =
+        tabTitle.getBoundingClientRect().height + titleOffset;
+
+      tab.style.setProperty("--min-tab-height", `${tabTitleHeight}px`);
+      tab.style.setProperty("--max-tab-height", `${tabHeight}px`);
+
+      this.timers_[index].style.setProperty(
+        "--min-tab-height",
+        `${tabTitleHeight}px`
+      );
+      this.timers_[index].style.setProperty(
+        "--max-tab-height",
+        `${tabHeight}px`
+      );
     });
   }
 
